5c76f1466f8c45940e90b34d5cf4df226a1f452f
Rhetteric <110702124+Rhetteric@users.noreply.github.com>
2022-09-03 21:47:47 -0400

    Create install_alt.sh

---
+++ train/install_alt.sh
@@ -0,0 +1,4 @@
+pip install wheel matplotlib
+pip uninstall torch torchvision torchaudio
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+apt -y install libsndfile1
