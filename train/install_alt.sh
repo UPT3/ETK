3d0cdc4ed9b6f8900754db9846d9cdaff8710576
heteric <110702124+Rhetteric@users.noreply.github.com>
2022-09-05 22:35:27 -0400

    Update install_alt.sh

--- train/install_alt.sh
+++ train/install_alt.sh
@@ -1,4 +1,11 @@
-pip install wheel matplotlib
-pip uninstall torch torchvision torchaudio
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-apt -y install libsndfile1
+echo "Installing, this wil take about 8 minutes."
+apt-get update
+echo "installing 7z"
+apt-get install -qq p7zip-full p7zip-rar libsndfile1
+echo "updating pip"
+pip install --upgrade pip
+echo "installing nnsvs main branch"
+pip install -I https://github.com/nnsvs/nnsvs/tarball/master
+echo "installing everything else (this will take a bit...)"
+pip install -I https://github.com/MattShannon/bandmat/archive/master.zip wheel numpy cython==0.29.27 optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper wheel matplotlib mlflow utaupy tqdm pydub pyyaml natsort github-clone tbb gdown torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+echo "Finished"
