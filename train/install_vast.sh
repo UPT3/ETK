4fc07a129bfdd03f4fa92b47de034ac736b0d554
heteric <110702124+Rhetteric@users.noreply.github.com>
2022-09-05 22:38:30 -0400

    Update install_vast.sh

--- train/install_vast.sh
+++ train/install_vast.sh
@@ -1,12 +1,12 @@
 # for use on VAST with python:3.8-buster image.
 echo "Installing, this wil take about 8 minutes."
-apt-get update
+apt-get update &> /dev/null
 echo "installing 7z"
-apt-get install -qq p7zip-full libsndfile1
+apt-get install -qq p7zip-full libsndfile1 &> /dev/null
 echo "updating pip"
-pip install --upgrade pip
+pip install --upgrade pip &> /dev/null
 echo "installing nnsvs main branch"
-pip install -I https://github.com/nnsvs/nnsvs/tarball/master
+pip install -I https://github.com/nnsvs/nnsvs/tarball/master &> /dev/null
 echo "installing everything else (this will take a bit...)"
-pip install -I https://github.com/MattShannon/bandmat/archive/master.zip wheel numpy cython==0.29.27 optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper wheel matplotlib mlflow utaupy tqdm pydub pyyaml natsort github-clone tbb gdown torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+pip install -I https://github.com/MattShannon/bandmat/archive/master.zip wheel numpy cython==0.29.27 optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper wheel matplotlib mlflow utaupy tqdm pydub pyyaml natsort github-clone tbb gdown torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116 &> /dev/null
 echo "Finished"
