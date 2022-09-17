66be888fb38d69c70b4689272de3c0370373e6ca
heteric <110702124+Rhetteric@users.noreply.github.com>
2022-09-17 19:18:04 -0400

    Update install_vast.sh

--- train/install_vast.sh
+++ train/install_vast.sh
@@ -8,5 +8,5 @@ pip install --upgrade pip &> /dev/null
 echo "installing nnsvs main branch"
 pip install -I https://github.com/nnsvs/nnsvs/tarball/master &> /dev/null
 echo "installing everything else (this will take a bit...)"
-pip install -I https://github.com/MattShannon/bandmat/archive/master.zip wheel numpy cython==0.29.27 optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper wheel matplotlib mlflow utaupy tqdm pydub pyyaml natsort github-clone tbb gdown "protobuf<3.20,>=3.9.2" setuptools==59.8.0 torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116 &> /dev/null
+pip install -I https://github.com/MattShannon/bandmat/archive/master.zip wheel numpy==1.22.4 cython==0.29.27 optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper wheel matplotlib mlflow utaupy tqdm pydub pyyaml natsort github-clone tbb gdown "protobuf<3.20,>=3.9.2" setuptools==59.8.0 torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116 &> /dev/null
 echo "Finished"
