d9b97ba7d6adc8e78aabf2c38a9881108c71efce
heteric <110702124+Rhetteric@users.noreply.github.com>
2022-09-18 10:05:42 -0400

    Update install.sh

--- train/install.sh
+++ train/install.sh
@@ -4,8 +4,10 @@ echo "installing 7z"
 DEBIAN_FRONTEND=noninteractive apt-get install -qq p7zip-full p7zip-rar
 echo "updating pip"
 pip install --upgrade pip
+echo "installing PyTorch"
+pip install -I torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 echo "installing nnsvs main branch"
 pip install -I https://github.com/nnsvs/nnsvs/tarball/master
 echo "installing everything else (this will take a bit...)"
-pip install -I https://github.com/MattShannon/bandmat/archive/master.zip wheel numpy==1.22.4 cython==0.29.27 optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper mlflow utaupy tqdm pydub pyyaml natsort github-clone tbb gdown "joblib==1.1.0" torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+pip install -I https://github.com/MattShannon/bandmat/archive/master.zip wheel numpy==1.21.6 cython==0.29.27 optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper mlflow utaupy tqdm pydub pyyaml natsort github-clone tbb gdown "joblib==1.1.0"
 echo "Finished"
