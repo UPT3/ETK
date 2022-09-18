461cd152c83a703d3a522c32e5c92eda8459310d
heteric <110702124+Rhetteric@users.noreply.github.com>
2022-09-18 11:08:12 -0400

    Update install.sh

--- train/install.sh
+++ train/install.sh
@@ -5,7 +5,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install -qq p7zip-full p7zip-rar
 echo "updating pip"
 pip install --upgrade pip
 echo "installing PyTorch"
-pip install -I torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+pip install -I torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 echo "installing nnsvs main branch"
 pip install -I https://github.com/nnsvs/nnsvs/tarball/master
 echo "installing everything else (this will take a bit...)"
