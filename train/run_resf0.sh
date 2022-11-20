a107a03b905091afbf2146eb0d83b29d9dee6857
heteric <110702124+hetteric@users.noreply.github.com>
2022-11-19 22:58:55 -0500

    Update run_resf0.sh

--- train/run_resf0.sh
+++ train/run_resf0.sh
@@ -89,24 +89,15 @@ if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
     . $NNSVS_COMMON_ROOT/train_resf0_acoustic.sh || exit 1;
 fi
 
-if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
-    echo ""
-    echo "###"
-    echo "stage 5: Feature generation"
-    echo "###"
-    echo ""
-    . $NNSVS_COMMON_ROOT/generate.sh || exit 1;
-fi
-
 # Skipping synthesis step.
 
 # Postfilter stuff
-if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
+if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
     echo "stage 6: Prepare input/output features for post-filter"
     . $NNSVS_COMMON_ROOT/prepare_postfilter.sh
 fi
 
-if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
+if [ ${stage} -le 6 ] && [ ${stop_stage} -ge 6 ]; then
     echo "stage 7: Training post-filter"
     . $NNSVS_COMMON_ROOT/train_postfilter.sh
 fi
