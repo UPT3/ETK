c6ebee50b7c186fd0a67777020db191c69716d98
heteric <110702124+hetteric@users.noreply.github.com>
2022-12-06 14:16:07 -0500

    Update install.sh

--- train/install.sh
+++ train/install.sh
@@ -12,5 +12,5 @@ echo "installing nnsvs 0.0.3"
 pip install nnsvs &> /dev/null
 
 echo "installing everything else (this will take a bit...)"
-pip install --no-cache-dir -U https://github.com/MattShannon/bandmat/archive/master.zip git+https://github.com/r9y9/ParallelWaveGAN@nnsvs wheel numpy scipy optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper mlflow utaupy tqdm pydub pyyaml natsort github-clone tbb gdown "joblib" "tensorboard<2.9,>=2.8" "typing-extensions<4.2.0,>=3.7.4" "protobuf<3.20,>=3.9.2" "cython==0.29.27" "setuptools<60" &> /dev/null
+pip install --no-cache-dir -U https://github.com/MattShannon/bandmat/archive/master.zip git+https://github.com/r9y9/ParallelWaveGAN@nnsvs wheel optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper mlflow numpy scipy utaupy tqdm pydub pyyaml natsort github-clone tbb gdown "joblib" "tensorboard<2.9,>=2.8" "typing-extensions<4.2.0,>=3.7.4" "protobuf<3.20,>=3.9.2" "cython==0.29.27" "setuptools<60" &> /dev/null
 echo "Finished"
