d410e4339bef4986736ddfb046df22fbcf8068bc
heteric <110702124+hetteric@users.noreply.github.com>
2022-12-09 16:15:17 -0500

    Update install_colab_local.sh

--- train/install_colab_local.sh
+++ train/install_colab_local.sh
@@ -3,14 +3,14 @@ sudo apt-get update
 echo "installing 7z"
 sudo apt-get install -qq p7zip-full p7zip-rar
 #echo "updating pip"
-pip install --upgrade pip &> /dev/null
+pip install --upgrade pip
 #echo "installing PyTorch"
 #pip install -I torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 #echo "installing nnsvs main branch"
 #pip install https://github.com/nnsvs/nnsvs/tarball/master &> /dev/null
 echo "installing nnsvs 0.0.3"
-pip install nnsvs &> /dev/null
+pip install nnsvs
 
 echo "installing everything else (this will take a bit...)"
-pip install --no-cache-dir -U https://github.com/MattShannon/bandmat/archive/master.zip git+https://github.com/r9y9/ParallelWaveGAN@nnsvs wheel optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper mlflow numpy scipy utaupy tqdm pydub pyyaml natsort github-clone tbb gdown "joblib" "tensorboard<2.9,>=2.8" "typing-extensions<4.2.0,>=3.7.4" "protobuf<3.20,>=3.9.2" "cython==0.29.27" "setuptools<60" &> /dev/null
+pip install --no-cache-dir -U https://github.com/MattShannon/bandmat/archive/master.zip git+https://github.com/r9y9/ParallelWaveGAN@nnsvs wheel optuna "hydra-core >= 1.1.0, < 1.2.0" "hydra_colorlog >= 1.1.0" hydra-optuna-sweeper mlflow numpy scipy utaupy tqdm pydub pyyaml natsort github-clone tbb gdown "joblib" "tensorboard<2.9,>=2.8" "typing-extensions<4.2.0,>=3.7.4" "protobuf<3.20,>=3.9.2" "cython==0.29.27" "setuptools<60"
 echo "Finished"
